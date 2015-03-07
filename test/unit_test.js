598696c6ae825de460419accd89ec1b7da7691e4
Arun <achandr8@ncsu.edu>
2015-03-07 16:23:22 -0500

    Second commit

---
+++ test/unit_test.js
@@ -0,0 +1,19 @@
+
+var assert = require('assert')
+  , inc = require('./../subject').inc;
+
+	suite('inc', function() 
+    	{
+    		test('inc should return 2', function(done) 
+		{
+          		assert.equal(2, inc(1,undefined));
+			done();
+		});
+    		test('inc should return 2', function(done) 
+		{
+          		assert.equal(2, inc(-1,1));
+			done();
+		});
+    	});
+     
+
