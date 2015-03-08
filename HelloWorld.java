53246559c5de9e74852b4b5c42c57d356e70d300
Arun <achandr8@ncsu.edu>
2015-03-08 01:24:13 -0500

    Java file

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		String[] field = { "Hi", "there", "How", "are", "you"};
+		String s = "";
+		for (int i = 0; i < field.length; ++i)
+		{
+			s = s + field[i];
+		}
+		System.out.println(s);
+
+		System.out.println("Hello, World");
+																			        }
+}
